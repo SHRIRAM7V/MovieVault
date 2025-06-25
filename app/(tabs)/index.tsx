06761c1ecf3ecd3dba6df4489c314cd018b91973
SHRIRAM7V <230121.IT@rmkec.ac.in>
2025-06-25 14:00:25 +0530

    setup the fetching and basic styling for text (name)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,66 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, View } from "react-native";
+import { fetchPets } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import SearchBar from "../../components/SearchBar";
 export default function Index() {
+  const router = useRouter();
+  const {
+    data: animals,
+    loading: animloading,
+    error: animerror,
+  } = useFetch(() => fetchPets({ query: "" }));
 
-  const router=useRouter();
+  console.log(animals);
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
       <ScrollView showsVerticalScrollIndicator={false} className="flex-1 px-5">
         <Image source={icons.logo} className="mt-16 mb-5 w-12 h-10 mx-auto" />
-        <SearchBar 
-        onPress={()=>router.push("/search")}
-        placeholder="Search for a movie"
-        />
+        {animloading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self center"
+          />
+        ) : animerror ? (
+          <Text>
+            Error:{animerror?.message}.Failed to fetch in the Index page
+          </Text>
+        ) : (
+          <View>
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+            <Text className="text-lg text-white font-bold mt-5 mb-3">Pets</Text>
+            <FlatList
+              data={animals}
+              renderItem={({ item }) => (
+                <Text className="text-white">{item.name}</Text>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              columnWrapperStyle={{
+                justifyContent: "flex-start",
+                marginBottom: 10,
+                gap: 20,
+                paddingRight: 5,
+              }}
+              className="mt-2 pb-32"
+              numColumns={3}
+              scrollEnabled={false}
+            />
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -1,15 +1,17 @@
 export const PETFINDER_CONFIG = {
   BASE_URL: "https://api.petfinder.com/v2/animals",
   headers: {
-    accept: "application/json",
+    // accept: "application/json",
     Authorization: `Bearer ${process.env.EXPO_PUBLIC_PETFINDER_TOKEN}`,
   },
 };
 
 export const fetchPets = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `${PETFINDER_CONFIG}?limit=20&type=${encodeURIComponent(query)}`
-    : `${PETFINDER_CONFIG}?limit=20`;
+    ? `${PETFINDER_CONFIG.BASE_URL}?limit=20&type=${encodeURIComponent(query)}`
+    : `${PETFINDER_CONFIG.BASE_URL}?limit=20`;
+
+  console.log(endpoint);
   const response = await fetch(endpoint, {
     method: "GET",
     headers: PETFINDER_CONFIG.headers,
@@ -17,7 +19,17 @@ export const fetchPets = async ({ query }: { query: string }) => {
   if (!response.ok) {
     throw new Error(`Failed to fetch movies: ${response.statusText}`);
   }
+  console.log(response);
 
   const data = await response.json();
-  return data.results;
+  //   const data = response;
+  //   console.log(data)
+  //   return data.results;
+  //   console.log(data);
+  console.log(JSON.stringify(data, null, 2));
+  //   console.log("Parsed JSON:", data); //  Check full object
+  //   console.log("Animals array:", data.animals); // Specific key you need
+//   console.log(data.animals);
+  return data.animals;
+  //   return data;
 };
